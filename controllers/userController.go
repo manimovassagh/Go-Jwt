0798c6edf40e00d479810ce5085bab3a0cdb87de
manimovassagh <mani.mgh@gmail.com>
2022-09-02 18:12:07 +0200

    done

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,8 +1,34 @@
 package controllers
 
+import (
+	"fmt"
+	"net/http"
 
-func SignUp(){
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
 
+func SignUp() {
 
-	
+	// get The Email and password of request body
+	var body struct {
+		Email    string
+		Password string
+	}
+
+	c.Bind(&body) != nil{
+		c.JSON(http.StatusBadRequest,gin.H{
+			"error":"OOpss You have Error in parsin Body"
+		}),
+	}
+	//Hash the  password
+	hash,err:=bcrypt.GenerateFormPassword([]byte(body.Password),10)
+
+	if err !=nil{
+		fmt.println("You have error in Hashing password")
+	}
+	// Create The user
+
+	//Respond
+
 }
