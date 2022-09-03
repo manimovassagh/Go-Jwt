088881b7919c0c6a0da97731551b4e51cb25b251
Mani Movassagh <mani.mgh@gmail.com>
2022-09-03 16:00:54 +0200

    fix path

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -3,8 +3,13 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/jwt-project/initializers"
+	"github.com/jwt-project/models"
 )
 
 func RequireAuth(c *gin.Context) {
@@ -18,12 +23,37 @@ func RequireAuth(c *gin.Context) {
 	}
 
 	//Decode/validate cookie
-	//parse takes the
-	//check the expiration
+	// Parse takes the token string and a function for looking up the key. The latter is especially
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
 
-	//find the user with token sub
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(os.Getenv("SECRET")), nil
+	})
 
-	//continue
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 
-	c.Next()
+		//check the expiration
+		if float64(time.Now().Unix()) >["exp"].(float64){
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+		//find the user with token sub
+		var user models.User
+		initializers.DB.First(&user,claims["sub"])
+		if user.ID==0{
+
+		}
+		//Attach to requests
+		//continue
+
+		c.Next()
+		fmt.Println(claims["foo"], claims["nbf"])
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	
 }
